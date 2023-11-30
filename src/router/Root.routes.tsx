606a408825f13ee98371e7b5e70276c00927612e
SergioBurlaka <burlaka01@gmail.com>
2023-11-30 12:13:54 +0200

    navigate to admin path by default

--- src/router/Root.routes.tsx
+++ src/router/Root.routes.tsx
@@ -1,18 +1,26 @@
 import type { FC } from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import { HomeNavigation } from "../modules/Home/HomeNavigation";
 
+import { Book } from "../modules/Books/Book";
+
 export const RootRoutes: FC = () => {
+
+
+
   return (
     <Router>
       <HomeNavigation />
       <Routes>
         {/* <Route  path="/" element={<div>home</div>} /> */}
-        <Route index element={<div>home </div>} />
+        {/* <Route index element={<div>home </div>} /> */}
 
-        <Route index path="admin" element={<div>admin </div>} />
+        <Route index element={<Navigate to={'admin'} />} />
+
+
+        <Route index path="admin" element={<Book />} />
         <Route path="manager" element={<div>manager </div>} />
         <Route path="user" element={<div>user </div>} />
 
