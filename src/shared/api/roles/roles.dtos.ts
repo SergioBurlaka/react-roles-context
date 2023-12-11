63404f681389db54540e1344b7f4eebfb77e0784
SergioBurlaka <burlaka01@gmail.com>
2023-12-11 19:09:31 +0200

    update roles

--- src/shared/api/roles/roles.dtos.ts
+++ src/shared/api/roles/roles.dtos.ts
@@ -1,4 +1,3 @@
-import { z } from 'zod';
 
 export enum DefaultUserRoles {
   ROLE_USER = 'ROLE_USER',
@@ -16,16 +15,3 @@ export type AllUserRolesType = DefaultUserRoles | UserRoles;
 
 export const clientUserRoles = [UserRoles.ROLE_CLIENT_MANAGER, UserRoles.ROLE_CLIENT_SUPERVISOR];
 
-export const roleDto = z.object({
-  id: z.number(),
-  name: z.string(),
-  description: z.string(),
-  created_at: z.string(),
-  updated_at: z.string(),
-});
-export type RoleDto = z.infer<typeof roleDto>;
-
-export const rolesResponseDto = z.object({
-  roles: z.array(roleDto),
-});
-export type RolesResponseDto = z.infer<typeof rolesResponseDto>;
